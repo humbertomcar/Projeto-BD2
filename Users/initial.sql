81c9f050f46dfc9dd7c86826e91dc9b564d80c2b
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-10-26 20:42:07 -0300

    Users done

--- Users/initial.sql
+++ Users/initial.sql
@@ -1,1 +1,10 @@
+USE Restaurante;
 
+CREATE USER 'administrador'@'localhost' IDENTIFIED BY '123';
+GRANT ALL ON Restaurante.* TO 'administrador'@'localhost';
+
+CREATE USER 'gerente'@'localhost' IDENTIFIED BY '123';
+GRANT SELECT, UPDATE, DELETE ON Restaurante.* TO 'gerente'@'localhost';
+
+CREATE USER 'funcionario'@'localhost' IDENTIFIED BY '123';
+GRANT SELECT, INSERT ON Restaurante.* TO 'funcionario'@'localhost';
