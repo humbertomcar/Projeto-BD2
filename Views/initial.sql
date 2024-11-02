6bcdfb65c33aa5c61dbb1a9e39a683e11f75aab8
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-11-01 21:15:36 -0300

    2 others views done

--- Views/initial.sql
+++ Views/initial.sql
@@ -11,4 +11,31 @@ JOIN
 GROUP BY 
     c.nome;
 
-SELECT * FROM VendasPorCliente
+CREATE VIEW TotalGastoPorCliente AS
+SELECT 
+    c.nome AS nome_cliente,
+    SUM(v.valor) AS total_gasto
+FROM 
+    cliente c
+JOIN 
+    venda v ON c.id_cliente = v.id_cliente
+GROUP BY 
+    c.nome;
+
+SELECT * FROM TotalGastoPorCliente;
+
+CREATE VIEW ClientesEVendas AS
+SELECT 
+    c.nome AS nome_cliente,
+    COUNT(v.id_venda) AS total_vendas
+FROM 
+    cliente c
+JOIN 
+    venda v ON c.id_cliente = v.id_cliente
+GROUP BY 
+    c.nome
+ORDER BY 
+    c.nome ASC;
+    
+Select * from ClientesEVendas;
+
