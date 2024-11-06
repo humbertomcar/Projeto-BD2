696b35330f2bdc3b3764c5b723bec25eb4264427
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-11-06 08:59:06 -0300

    segundo trigger done

---
+++ Triggers/validade.sql
@@ -0,0 +1,23 @@
+USE Restaurante;
+
+DELIMITER $$
+
+CREATE TRIGGER verificaValidadeIngrediente
+    AFTER UPDATE ON ingredientes
+    FOR EACH ROW
+BEGIN
+    DECLARE validade_vencida BOOLEAN;
+
+    -- ve se o ingrediente atualizado está vencido
+    SET validade_vencida = (NEW.validade < CURDATE());
+
+    IF validade_vencida THEN
+        -- att a disponibilidade dospratos que usam o ingrediente vencido
+    UPDATE prato p
+        JOIN usos u ON p.id_prato = u.id_prato
+        SET p.disponibilidade = 0
+    WHERE u.id_ingrediente = NEW.id_ingrediente;
+END IF;
+END$$
+
+DELIMITER ;
