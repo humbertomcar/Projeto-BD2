5535e970b8d95e5e19c59fe0d7bbd0ecd45e2016
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-11-06 23:16:32 -0300

    validade v2

--- Triggers/validade.sql
+++ Triggers/validade.sql
@@ -2,26 +2,20 @@ USE Restaurante;
 
 DELIMITER $$
 
-CREATE TRIGGER verificaValidadeIngrediente
+CREATE TRIGGER atualizaDisponibilidadePrato
     AFTER UPDATE ON ingredientes
     FOR EACH ROW
 BEGIN
-    DECLARE validade_vencida BOOLEAN;
-
-    -- ve se o ingrediente atualizado está vencido
-    SET validade_vencida = (NEW.validade < CURDATE());
-
-    IF validade_vencida THEN
-        -- att a disponibilidade dospratos que usam o ingrediente vencido
-    UPDATE prato p
-        JOIN usos u ON p.id_prato = u.id_prato
-        SET p.disponibilidade = 0
-    WHERE u.id_ingrediente = NEW.id_ingrediente;
-END IF;
+    -- Verifica se o ingrediente atualizado está vencido
+    IF NEW.validade < CURDATE() THEN
+        -- Atualiza a disponibilidade dos pratos que usam o ingrediente vencido para indisponível (0)
+        UPDATE prato p
+            JOIN usos u ON p.id_prato = u.id_prato
+            SET p.disponibilidade = 0
+        WHERE u.id_ingrediente = NEW.id_ingrediente;
+    END IF;
 END$$
 
 DELIMITER ;
 
 SELECT * FROM prato;
-
-SELECT * FROM ingredientes;
